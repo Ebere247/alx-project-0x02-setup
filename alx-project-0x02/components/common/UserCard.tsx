ba9b84576817a6b6be3e18d6bc290098f6402722
Ebere Onuobia <cynthiaonuobia@gmail.com>
2025-07-24 11:58:58 +0100

    UserCard Component

---
+++ alx-project-0x02/components/common/UserCard.tsx
@@ -0,0 +1,17 @@
+// components/common/UserCard.tsx
+import React from "react";
+import { UserProps } from "../../interfaces";
+
+const UserCard: React.FC<UserProps> = ({ name, email, address }) => {
+  return (
+    <div className="bg-white shadow rounded-lg p-4 border">
+      <h2 className="text-lg font-bold">{name}</h2>
+      <p className="text-sm text-gray-700">{email}</p>
+      <p className="text-sm text-gray-500">
+        {address.street}, {address.suite}, {address.city}, {address.zipcode}
+      </p>
+    </div>
+  );
+};
+
+export default UserCard;
