de6467d57508c20dac3fff1d8235fa2f630ece76
Ebere Onuobia <cynthiaonuobia@gmail.com>
2025-07-24 11:01:05 +0100

    Button Component

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import Button from "@/components/common/Button";
 
-const About: React.FC = () => {
+const AboutPage: React.FC = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">About Us</h1>
+    <div className="p-10 space-y-4">
+      <h1 className="text-2xl font-bold mb-4">About Us</h1>
+
+      <div className="flex gap-4 flex-wrap">
+        <Button label="Small Rounded-sm" size="small" shape="rounded-sm" />
+        <Button label="Medium Rounded-md" size="medium" shape="rounded-md" />
+        <Button label="Large Rounded-full" size="large" shape="rounded-full" />
+      </div>
     </div>
   );
 };
 
-export default About;
+export default AboutPage;
